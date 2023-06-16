4196d9fe3046e78473aeb7b14d2f95d199896683
AJlexand3r <Aliengoodgood@gmail.com>
2023-06-16 20:23:36 +0300

    1/113

---
+++ 1CommentYourJScode.js
@@ -0,0 +1,19 @@
+const Alexander = {  //начало
+    age: 27,
+    live: 'Minsk'
+  }
+  
+  const Alexander2 = {
+    skill: 'Low',
+    weight: 80
+  }
+  
+  const our = {
+    ...Alexander,
+    ...Alexander2
+  }                //конец
+  /*
+  Моя характеристика небольшая 
+  Алекснадр Бильдюк
+  */
+  console.table(our)
